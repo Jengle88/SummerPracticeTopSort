9c935910259c6e6886462584e07718ac30dc3c15
Anton Toropygin <71664787+TheAnton1@users.noreply.github.com>
2022-07-11 14:09:29 +0300

    Создана функция проверки графа на цикл

--- src/jvmMain/kotlin/utils/algorithm/GraphAlgorithm.kt
+++ src/jvmMain/kotlin/utils/algorithm/GraphAlgorithm.kt
@@ -7,6 +7,50 @@ import kotlin.collections.ArrayList
 
 object GraphAlgorithm {
 
+<<<<<<< Updated upstream
+=======
+    private fun addState(srcVertex: Vertex, dstVertex: Vertex? = null, action: Actions,
+                         protocol: ArrayList<State>) {
+        when (action) {
+            Actions.DOWN_TO_EDGE -> protocol.add(State(
+                getCurrentTime(),
+                "Down from vertex ${srcVertex.getName()} (id = ${srcVertex.getId()}) to ${dstVertex?.getName()} (id = ${dstVertex?.getId()})",
+                srcVertex.getId(),
+                dstVertex?.getId()))
+
+            Actions.GET_ORDER -> protocol.add(State(
+                getCurrentTime(),
+                "Vertex ${srcVertex.getName()} (id = ${srcVertex.getId()}) got order",
+                srcVertex.getId()
+            ))
+
+            Actions.ADDED_TO_STACK -> protocol.add(State(
+                getCurrentTime(),
+                "Vertex ${srcVertex.getName()} (id = ${srcVertex.getId()}) added to stack",
+                srcVertex.getId()
+            ))
+        }
+    }
+
+    private fun getCurrentTime(): String {
+        return SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date())
+    }
+
+    private fun checkGraphForCycle(graph: Graph, current: Vertex, visited: ArrayList<Vertex>, checkList: ArrayList<Boolean>) {
+        visited.add(current)
+        val edges = initEdges(graph, current)
+        for (edge in edges.values) {
+            if (edge !in visited) {
+                checkGraphForCycle(graph, edge, visited, checkList)
+            }
+            else {
+                checkList.add(false)
+            }
+        }
+        checkList.add(true)
+    }
+
+>>>>>>> Stashed changes
     private fun initEdges(graph: Graph, current: Vertex): MutableMap<Long, Vertex> {
         val vertexes = graph.getVertexes()
         val edges: MutableMap<Long, Vertex> = mutableMapOf()
@@ -31,7 +75,15 @@ object GraphAlgorithm {
     fun TopSort(graph: Graph): Map<Vertex, Int> {
         val stackOfVertexes: Stack<Vertex> = Stack()
         val vertexes = graph.getVertexes()
-        val visited: ArrayList<Vertex> = arrayListOf()
+        var visited: ArrayList<Vertex> = arrayListOf()
+        val checkList: ArrayList<Boolean> = arrayListOf()
+        for (vertex in vertexes) {
+            if (vertex !in visited) {
+                checkGraphForCycle(graph, vertex, visited, checkList)
+            }
+        }
+        if (false in checkList) return mapOf()
+        visited = arrayListOf()
         for (vertex in vertexes) {
             if (vertex !in visited) {
                 TopSortUtil(graph, vertex, visited, stackOfVertexes)
@@ -44,5 +96,50 @@ object GraphAlgorithm {
         }
         return result
     }
+<<<<<<< Updated upstream
+=======
+
+    private fun TopSortUtilActions(graph: Graph, current: Vertex, visited: ArrayList<Vertex>,
+                                   stackOfVertexes: Stack<Vertex>, protocol: ArrayList<State>) {
+        visited.add(current)
+        val edges: MutableMap<Long, Vertex> = initEdges(graph, current)
+        for (edge in edges.values) {
+            if (edge !in visited) {
+                addState(current, edge, Actions.DOWN_TO_EDGE, protocol)
+                this.TopSortUtilActions(graph, edge, visited, stackOfVertexes, protocol)
+            }
+        }
+        stackOfVertexes.add(current)
+        addState(current, action=Actions.ADDED_TO_STACK, protocol=protocol)
+    }
+
+    fun TopSortActions(graph: Graph): Pair<Map<Vertex, Int>, ArrayList<State>> {
+        val stackOfVertexes: Stack<Vertex> = Stack()
+        val protocol: ArrayList<State> = arrayListOf()
+        val vertexes = graph.getVertexes()
+        var visited: ArrayList<Vertex> = arrayListOf()
+        val checkList: ArrayList<Boolean> = arrayListOf()
+        for (vertex in vertexes) {
+            if (vertex !in visited) {
+                checkGraphForCycle(graph, vertex, visited, checkList)
+            }
+        }
+        if (false in checkList) return Pair(mapOf(), arrayListOf(State(getCurrentTime(), "Cycle was found, create another graph", 0)))
+        visited = arrayListOf()
+        for (vertex in vertexes) {
+            if (vertex !in visited) {
+                TopSortUtilActions(graph, vertex, visited, stackOfVertexes, protocol)
+            }
+        }
+        var order = 0
+        val result: MutableMap<Vertex, Int> = mutableMapOf()
+        while (stackOfVertexes.isNotEmpty()) {
+            val current = stackOfVertexes.pop()
+            result[current] = order++
+            addState(current, action=Actions.GET_ORDER, protocol=protocol)
+        }
+        return Pair(result, protocol)
+    }
+>>>>>>> Stashed changes
 }
 
