afd1e436f4032c64d99de43def1255d5e02cf446
Anton Toropygin <71664787+TheAnton1@users.noreply.github.com>
2022-07-12 13:15:30 +0300

    Добавлена проверка координат
    Проверка на неотрицательность координат; проверка на то, что вершины не перекрывают друг друга.
    Добавлено тестирование

--- src/jvmMain/kotlin/utils/parsing/Parser.kt
+++ src/jvmMain/kotlin/utils/parsing/Parser.kt
@@ -3,15 +3,19 @@ package utils.parsing
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import data.`object`.Graph
+import org.jetbrains.skia.Point
+import ui.GraphEditor.GraphCanvas.VertexVO
+import utils.getDistTo
 import java.io.File
 
 object Parser {
     fun checkGraphForValid(graph: Graph) : Boolean {
         val vertexes = graph.getVertexes()
         val ids:ArrayList<Long> = arrayListOf()
-        for (i in 0..vertexes.size-1) {
+        for (i in 0 until vertexes.size) {
             ids.add(vertexes[i].getId())
         }
+
         // проверка на одинаковые id
         if (ids.groupingBy { it }.eachCount().filter { it.value > 1 }.isNotEmpty()) return false
         // проверка на несуществующие рёбра
@@ -21,6 +25,25 @@ object Parser {
                 if (edge !in ids) return false
             }
         }
+
+        // проверка на неотрицательность координат
+        val centers: ArrayList<Point> = arrayListOf()
+        for (i in 0 until vertexes.size) {
+            centers.add(vertexes[i].getCenter())
+        }
+        for (center in centers) {
+            if (center.x <= VertexVO.radius || center.y <= VertexVO.radius) return false
+        }
+
+        // проверка на то, что вершины не перекрывают друг друга
+        for (centerSrc in centers) {
+            for (centerDst in centers) {
+                if (centerDst != centerSrc){
+                    if (centerSrc.getDistTo(centerDst) < VertexVO.radius) return false
+                }
+            }
+        }
+
         return true
     }
     fun readDataJSON(filePath: String): Graph {
