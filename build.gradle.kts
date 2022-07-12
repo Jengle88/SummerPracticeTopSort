1a5975195dd14a7fa5972011cfcb46be01deb01e
Anton Toropygin <71664787+TheAnton1@users.noreply.github.com>
2022-07-12 16:08:42 +0300

    Исправлена структура директорий для тестов

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 dependencies {
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 kotlin {
@@ -71,5 +72,8 @@ tasks {
         }
         from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
         from(sourceSets.main.get().output)
+    }
+    withType<Test> {
+        useJUnitPlatform()
     }
 }
