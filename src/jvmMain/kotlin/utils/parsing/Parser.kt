5577950219fc0ef05f0090ea0d6d0cb31ed85cb6
Anton Toropygin <71664787+TheAnton1@users.noreply.github.com>
2022-07-12 12:39:17 +0300

    Добавлена проверка на валидность считываемых данных
    Добавлено тестирование функции проверки считываемых данных

--- src/jvmMain/kotlin/utils/parsing/Parser.kt
+++ src/jvmMain/kotlin/utils/parsing/Parser.kt
@@ -6,6 +6,23 @@ import data.`object`.Graph
 import java.io.File
 
 object Parser {
+    fun checkGraphForValid(graph: Graph) : Boolean {
+        val vertexes = graph.getVertexes()
+        val ids:ArrayList<Long> = arrayListOf()
+        for (i in 0..vertexes.size-1) {
+            ids.add(vertexes[i].getId())
+        }
+        // проверка на одинаковые id
+        if (ids.groupingBy { it }.eachCount().filter { it.value > 1 }.isNotEmpty()) return false
+        // проверка на несуществующие рёбра
+        for (vertex in vertexes) {
+            val edges = vertex.getEdges()
+            for (edge in edges) {
+                if (edge !in ids) return false
+            }
+        }
+        return true
+    }
     fun readDataJSON(filePath: String): Graph {
         val builder = GsonBuilder()
         val gson = builder.create()
@@ -15,6 +32,7 @@ object Parser {
         if (graph.getVertexes() == null) {
             graph = Graph(arrayListOf())
         }
+        if (!checkGraphForValid(graph)) return Graph(arrayListOf())
         return graph
     }
 
